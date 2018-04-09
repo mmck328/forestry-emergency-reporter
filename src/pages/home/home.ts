d1b1d2a605eac9d512c32b7e60c03fa24ebffbd8
mmck328 <edamame1010@hotmail.co.jp>
2018-04-09 17:10:15 +0900

    [add] handle accelerometer

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { DeviceMotion, DeviceMotionAccelerationData } from '@ionic-native/device-motion'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
-
+  acceleration: DeviceMotionAccelerationData;
+  vector: number;
+  moved: boolean;
+  subscription: any;
+  constructor(public navCtrl: NavController, public platform: Platform, private deviceMotion: DeviceMotion) {
+    this.acceleration = {x: 0, y: 0, z: 0, timestamp: null};
+    this.vector = 9.8;
+    this.moved = false;
+    
+    this.platform.ready().then((readySource) => {
+      this.subscription = this.deviceMotion.watchAcceleration({frequency: 100}).subscribe((acceleration: DeviceMotionAccelerationData) => {
+        this.acceleration = acceleration;
+        this.vector = Math.sqrt(acceleration.x * acceleration.x + acceleration.y * acceleration.y + acceleration.z * acceleration.z);
+        this.moved = this.vector < 9.4 || 10.2 < this.vector;
+      });
+    })
+  };
 }
